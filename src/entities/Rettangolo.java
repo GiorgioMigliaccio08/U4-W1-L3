3612f557bf0375e3dac6372ab3bd8d8a6bbd8526
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-29 15:41:22 +0100

    Add class Rettangolo

---
+++ src/entities/Rettangolo.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Rettangolo {
+    private int altezza;
+    private int larghezza;
+}
+
+public Rettangolo(int altezza, int larghezza){
+    this.altezza = altezza;
+    this.larghezza = larghezza;
+}
+
+pubblic int calcolaPerimetro(){
+    return 2 * (altezza+larghezza);
+}
+
+public int calcolaArea() {
+    return altezza * larghezza;
+}
+
+public void stampaRettangolo() {
+        System.out.println("Altezza: " + altezza);
+        System.out.println("Larghezza: " + larghezza);
+        System.out.println("Area: " + calcolaArea());
+        System.out.println("Perimetro: " + calcolaPerimetro());
+        }
+        }
