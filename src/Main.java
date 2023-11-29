04abae0d27ecfd38c11be290c187d594e2a8efce
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-29 15:52:28 +0100

    Add modify Rettangolo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import entities.Rettangolo;
 public class Main {
+    public static void main(String[] args) {
+        Rettangolo r1 = new Rettangolo(5, 10);
+        Rettangolo r2 = new Rettangolo(3, 7);
 
+        System.out.println("Rettangolo 1:");
+        r1.stampaRettangolo();
+
+        System.out.println("Rettangolo 2:");
+        r2.stampaRettangolo();
+
+        stampaDueRettangoli(r1, r2);
+    }
+
+    public static void stampaDueRettangoli(Rettangolo r1, Rettangolo r2) {
+        System.out.println("Rettangolo 1:");
+        r1.stampaRettangolo();
+        System.out.println("Rettangolo 2:");
+        r2.stampaRettangolo();
+
+        int sommaAree = r1.calcolaArea() + r2.calcolaArea();
+        int sommaPerimetri = r1.calcolaPerimetro() + r2.calcolaPerimetro();
+        System.out.println("Somma delle aree: " + sommaAree);
+        System.out.println("Somma dei perimetri: " + sommaPerimetri);
+    }
 }
--- src/entities/Rettangolo.java
+++ src/entities/Rettangolo.java
@@ -3,25 +3,24 @@ package entities;
 public class Rettangolo {
     private int altezza;
     private int larghezza;
-}
 
-public Rettangolo(int altezza, int larghezza){
-    this.altezza = altezza;
-    this.larghezza = larghezza;
-}
+    public Rettangolo(int altezza, int larghezza) {
+        this.altezza = altezza;
+        this.larghezza = larghezza;
+    }
 
-pubblic int calcolaPerimetro(){
-    return 2 * (altezza+larghezza);
-}
+    public int calcolaPerimetro() {
+        return 2 * (altezza + larghezza);
+    }
 
-public int calcolaArea() {
-    return altezza * larghezza;
-}
+    public int calcolaArea() {
+        return altezza * larghezza;
+    }
 
-public void stampaRettangolo() {
+    public void stampaRettangolo() {
         System.out.println("Altezza: " + altezza);
         System.out.println("Larghezza: " + larghezza);
         System.out.println("Area: " + calcolaArea());
         System.out.println("Perimetro: " + calcolaPerimetro());
-        }
-        }
+    }
+}
