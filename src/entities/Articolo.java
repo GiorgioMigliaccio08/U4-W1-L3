399d4b6204d71fa76b30347a6a51a270d18f4c19
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-29 16:31:47 +0100

    Add Class new Articolo

--- src/entities/Articolo.java
+++ src/entities/Articolo.java
@@ -6,5 +6,44 @@ public class Articolo {
     private double prezzo;
     private int pezziDisponibili;
 
+    public Articolo(int codiceArticolo, String destinazioneArticolo, double prezzo, int pezziDisponibili) {
+        this.codiceArticolo = codiceArticolo;
+        this.destinazioneArticolo = destinazioneArticolo;
+        this.prezzo = prezzo;
+        this.pezziDisponibili = pezziDisponibili;
+    }
 
+
+    public int getCodiceArticolo() {
+        return codiceArticolo;
+    }
+
+    public void setCodiceArticolo(int codiceArticolo) {
+        this.codiceArticolo = codiceArticolo;
+    }
+
+    public String getDestinazioneArticolo() {
+        return destinazioneArticolo;
+    }
+
+    public void setDestinazioneArticolo(String destinazioneArticolo) {
+        this.destinazioneArticolo = destinazioneArticolo;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public int getPezziDisponibili() {
+        return pezziDisponibili;
+    }
+
+    public void setPezziDisponibili(int pezziDisponibili) {
+        this.pezziDisponibili = pezziDisponibili;
+    }
 }
+
